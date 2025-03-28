f1fddbd5a8d4d19c9688c5d7bbed1a7702093a66
KurisuNya <cuizhu1216@gmail.com>
2025-03-28 20:44:55 +0800

    feat(example): add yolov12 example

---
+++ example/yolov12/summary.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from kurisuinfo import summary
+from loguru import logger
+
+from kurisunet import get_module
+
+
+if __name__ == "__main__":
+    dir = Path(__file__).parent
+    cfg = {
+        "path": "./net.yaml",
+        "name": "YOLOv12",
+        "kwargs": {"in_ch": 3, "class_num": 80, "scale": "n"},
+        "input_shape": (1, 3, 512, 512),
+    }
+
+    module = get_module(cfg["name"], kwargs=cfg["kwargs"], config=dir / cfg["path"])
+    module_summary = str(summary(module, cfg["input_shape"], verbose=0))
+    logger.info(f"Summary of {cfg['name']}:\n{module_summary}")
