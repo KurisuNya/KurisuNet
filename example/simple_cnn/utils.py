b19b86eef845f7b5e7c27d3a477078509ee4e969
KurisuNya <cuizhu1216@gmail.com>
2025-03-28 16:37:16 +0800

    feat(example): update simpleCNN example

--- example/simple_cnn/utils.py
+++
@@ -1,8 +0,0 @@
-def make_divisible(v, divisor, min_value=None):
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
